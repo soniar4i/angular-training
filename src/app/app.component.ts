ecb6ccfa0a597b735dcb09eb1b24c254be9e577a
Sonia Conde <soniar4i@gmail.com>
2018-12-31 17:05:45 +0100

    Experiment with different built in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,11 @@ import { Course } from './model/course';
 })
 export class AppComponent {
   courses = COURSES;
+  startDate = new Date(2018, 0, 1);
+  title = COURSES[0].description;
+  price = 9.992049349032;
+  rate = 0.63;
+  course = COURSES[0];
 
   onCourseSelected(course: Course) {
     console.log(course);
